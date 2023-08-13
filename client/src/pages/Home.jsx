d43cab33dfd5d35a8fb96ba36946e47b9b2c0fff
Maximiliano Cassol <maximilianocassol@gmail.com>
2023-08-13 16:33:08 -0300

    pagination

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,13 +6,15 @@ import axios from "axios";
 
 const Home = () => {
   const [posts, setPosts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1); // Página actual
+  const postsPerPage = 5;
 
   const cat = useLocation().search
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await axios.get(`/posts${cat}`);
+        const res = await axios.get(`/posts${cat}`)
         setPosts(res.data);
       } catch (err) {
         console.log(err);
@@ -20,6 +22,14 @@ const Home = () => {
     };
     fetchData();
   }, [cat]);
+
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
 
   const getText = (html) =>{
     const doc = new DOMParser().parseFromString(html, "text/html")
@@ -30,7 +40,7 @@ const Home = () => {
   return (
     <div className="home">
       <div className="posts">
-        {posts.map((post) => (
+        {currentPosts.map((post) => (
           <div className="post" key={post.id}>
             <div className="img">
               {post.img
@@ -38,13 +48,20 @@ const Home = () => {
               : <img src={`../upload/${post.img}`} alt="pic" />}
             </div>
             <div className="content">
-              <Link className="link" to={`/post/${post.id}`}>
+              <Link className="link" to={`/posts/${post.id}`}>
                 <h1>{post.title}</h1>
               </Link>
               <p>{getText(post.desc)}</p>
-              <button>Read More</button>
-            </div>
+              {/* <button>Read More</button> */}
+            </div>            
           </div>
+        ))}
+      </div>
+      <div className="pagination">
+        {Array.from({ length: Math.ceil(posts.length / postsPerPage) }, (_, index) => (
+          <button key={index} onClick={() => paginate(index + 1)}>
+            {index + 1}
+          </button>
         ))}
       </div>
     </div>
